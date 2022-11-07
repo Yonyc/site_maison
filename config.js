e7ebf747cb2a96ffeddf92bc3f8bebbd72953a34
Yonyc <arnaudwery24@gmail.com>
2022-11-07 08:15:14 +0100

    Changed repo

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const PORT = 3000; // Main website port
-export const PORTS = [1234, 2342]; // Ports redirected on PORT const
+export const PORT = 80; // Main website port
+export const PORTS = []; // Ports redirected on PORT const
 export const httpsCreditentials = null;
 
 export const TITLE = "Wery Page";
