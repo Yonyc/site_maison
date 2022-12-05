22b56d5b1b58c4a95583bd6520c79949570089c5
Yonyc <arnaudwery24@gmail.com>
2022-12-05 18:23:38 +0100

    Added names and animations

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const PORT = 80; // Main website port
+export const PORT = 801; // Main website port
 export const PORTS = []; // Ports redirected on PORT const
 export const httpsCreditentials = null;
 
