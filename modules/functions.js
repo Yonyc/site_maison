81fb134615aabbb5d6c4d2ef77945e4304e9e9d7
Dwarlex <alexandrewery23@gmail.com>
2023-03-11 20:01:07 +0100

    Update 'modules/functions.js'

--- modules/functions.js
+++ modules/functions.js
@@ -1,7 +1,7 @@
 import { exec } from "child_process";
 
 export async function WOL(MAC) {
-    if (!MAC || !(/[0-9a-f]{1,2}([\.:-])(?:[0-9a-f]{1,2}\1){4}[0-9a-f]{1,2}/.test(MAC))) return Promise.reject({ msg: "Invali MAC address" });
+    if (!MAC || !(/[0-9a-fA-F]{1,2}([\.:-])(?:[0-9a-fA-F]{1,2}\1){4}[0-9a-fA-F]{1,2}/.test(MAC))) return Promise.reject({ msg: "Invali MAC address" });
     return new Promise((resolve, reject) => {
         exec("wakeonlan " + MAC, (error, stdout, stderr) => {
             if (error) {
