85e9854e3673d529367fd16c23ade424b0c15111
arlexTech <62621351+arlexTech@users.noreply.github.com>
2023-10-02 10:56:59 +0200

    Onlanwaker

--- modules/functions.js
+++ modules/functions.js
@@ -3,7 +3,7 @@ import { exec } from "child_process";
 export async function WOL(MAC) {
     if (!MAC || !(/[0-9a-fA-F]{1,2}([\.:-])(?:[0-9a-fA-F]{1,2}\1){4}[0-9a-fA-F]{1,2}/.test(MAC))) return Promise.reject({ msg: "Invali MAC address" });
     return new Promise((resolve, reject) => {
-        exec("wakeonlan -i 192.168.7.255 " + MAC, (error, stdout, stderr) => {
+        exec("ssh onlanwaker@192.168.7.1 'wakeonlan -i 192.168.7.255 " + MAC + "'", (error, stdout, stderr) => {
             if (error) {
                 return reject({ msg: error.message });
             }
