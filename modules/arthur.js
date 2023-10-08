87570bacce4ba97eeb829bb65cb2a3647ca24f0c
arlexTech <62621351+arlexTech@users.noreply.github.com>
2023-10-08 21:44:29 +0200

    Update arthur.js

--- modules/arthur.js
+++ modules/arthur.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { WOL } from "./functions.js";
 
-const alex = new express.Router();
+const arthur = new express.Router();
 
 export default arthur;
 
-alex.use("/wake", async (req, res) => {
+arthur.use("/wake", async (req, res) => {
     let r;
     r = await WOL("0C:9D-92-C1-93-EB").catch(m => r = m);
     res.send(r.msg).end();
