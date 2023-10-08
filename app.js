f5baad4adc749f904c85bd3295fe58caa31bee01
arnaud <37458412+Yonyc@users.noreply.github.com>
2023-10-08 21:47:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import pageGenerator from "./modules/pageGenerator.js";
 import { TITLE } from "./config.js";
 import alex from "./modules/alex.js";
 import arnaud from "./modules/arnaud.js";
+import arthur from "./modules/arthur.js";
 
 const app = new express.Router();
 
@@ -18,6 +19,7 @@ app.use("/api", api);
 
 app.use("/alex", alex);
 app.use("/arnaud", arnaud);
+app.use("/arnaud", arthur);
 
 /* app.use("/arnaud", (req, res) => {
     res.write(pageGenerator("./pages/arnaud.html", "Arnaud", ["/js/arnaud.js"]));
